3832f3f47c907a6a3f1b1bcda0f56b2f2cf2102c
Sander de Jong <sander@frontmen.nl>
2022-03-15 14:08:27 +0100

    Jobs on homepage

---
+++ components/JobGrid.js
@@ -0,0 +1,44 @@
+const JobGrid = ({ jobs }) => (
+  <div className="line grid grid-cols-5 gap-4 gap-y-10">
+    {jobs.map((job) => (
+      <a
+        key={job.id}
+        href={job.careers_url}
+        target="_blank"
+        rel="noreferrer"
+        className="h-40 border-t border-slate-200 pt-3"
+      >
+        <article>
+          <header>
+            <h3 className="text my-1.5 text-xl font-medium">{job.title}</h3>
+          </header>
+          <p className="mb-2 font-serif text-sm font-light">{job.location}</p>
+          <svg style={{ width: 24, height: 24 }} className="text-gray-400" viewBox="0 0 24 24">
+            <path
+              fill="currentColor"
+              d="M14,3V5H17.59L7.76,14.83L9.17,16.24L19,6.41V10H21V3M19,19H5V5H12V3H5C3.89,3 3,3.9 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V12H19V19Z"
+            />
+          </svg>
+        </article>
+      </a>
+    ))}
+    <a
+      href="https://www.iodigital.com/en/careers/jobs?expertise=Technology"
+      target="_blank"
+      rel="noreferrer"
+      className="h-40 bg-amber-600 p-2 pt-3 text-white hover:bg-amber-700"
+    >
+      <h3 className="text my-1.5 text-2xl font-medium">
+        View all <span className="font-serif">jobs</span>
+      </h3>
+      <svg style={{ width: 24, height: 24 }} className="text-white" viewBox="0 0 24 24">
+        <path
+          fill="currentColor"
+          d="M14,3V5H17.59L7.76,14.83L9.17,16.24L19,6.41V10H21V3M19,19H5V5H12V3H5C3.89,3 3,3.9 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V12H19V19Z"
+        />
+      </svg>
+    </a>
+  </div>
+)
+
+export default JobGrid
--- pages/api/jobs.js
+++ pages/api/jobs.js
@@ -7,29 +7,27 @@ export default async (req, res) => {
     offers: { offer: offers },
   } = await fetch(baseURL)
     .then((res) => res.text())
-    .then((res) => parser.parseStringPromise(res))
+    .then((res) => parser.parseStringPromise(res, { explicitArray: false }))
     .catch((err) => console.error(err))
-
-  const getFirstTrimmed = (arr) => arr.at(0).trim()
 
   const jobs = offers
     .map((offer) => ({
-      id: getFirstTrimmed(offer.id),
-      title: getFirstTrimmed(offer.title),
-      location: getFirstTrimmed(offer.location),
-      country: getFirstTrimmed(offer.country),
-      city: getFirstTrimmed(offer.city),
-      postal_code: getFirstTrimmed(offer.postal_code),
-      country_code: getFirstTrimmed(offer.country_code),
-      department: getFirstTrimmed(offer.department),
-      employment_type: getFirstTrimmed(offer.employment_type_code),
-      category: getFirstTrimmed(offer.category),
-      experience: getFirstTrimmed(offer.experience_code),
-      education: getFirstTrimmed(offer.education_code),
+      id: offer.id,
+      title: offer.title.trim(),
+      location: offer.location,
+      country: offer.country,
+      city: offer.city,
+      postal_code: offer.postal_code,
+      country_code: offer.country_code,
+      department: offer.department,
+      employment_type: offer.employment_type_code,
+      category: offer.category,
+      experience: offer.experience_code,
+      education: offer.education_code,
       tags: offer.tags,
-      min_hours: parseInt(getFirstTrimmed(offer.min_hours)),
-      max_hours: parseInt(getFirstTrimmed(offer.max_hours)),
-      careers_url: getFirstTrimmed(offer.careers_url),
+      min_hours: parseInt(offer.min_hours),
+      max_hours: parseInt(offer.max_hours),
+      careers_url: offer.careers_url,
       created_at: new Date(offer.created_at.at(0)),
       updated_at: new Date(offer.updated_at.at(0)),
       published_at: new Date(offer.published_at.at(0)),
