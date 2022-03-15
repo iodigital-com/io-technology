d10b44014ac7eabcb3e36f732d4bec485a90b656
Randynamic <randy@randynamic.com>
2022-03-15 11:27:31 +0100

    Added black header => still need to go full width

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -11,7 +11,7 @@ const LayoutWrapper = ({ children }) => {
   return (
     <SectionContainer>
       <div className="flex h-screen flex-col justify-between">
-        <header className="flex items-center justify-between py-10">
+        <header className="flex items-center justify-between bg-black py-10 text-white">
           <div>
             <Link href="/" aria-label={siteMetadata.headerTitle}>
               <div className="flex items-center justify-between">
@@ -34,13 +34,12 @@ const LayoutWrapper = ({ children }) => {
                 <Link
                   key={link.title}
                   href={link.href}
-                  className="p-1 font-medium text-gray-900 dark:text-gray-100 sm:p-4"
+                  className="p-1 font-medium text-white dark:text-gray-100 sm:p-4"
                 >
                   {link.title}
                 </Link>
               ))}
             </div>
-            <ThemeSwitch />
             <MobileNav />
           </div>
         </header>
