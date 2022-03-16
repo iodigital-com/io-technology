c9c5f9691b1f3520cfd1f0c28dc28efd1eda8a5f
Randynamic <randy@randynamic.com>
2022-03-16 13:29:02 +0100

    Blog title

--- pages/index.js
+++ pages/index.js
@@ -48,8 +48,10 @@ export default function Home({ posts, videos, jobs, authors }) {
       <PageSEO title={siteMetadata.title} description={siteMetadata.description} />
       <div className="divide-y divide-gray-200 bg-io_blue-500 p-16 text-white">
         <div className="space-y-2 pt-6 pb-8 md:space-y-5">
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            Blog
+          <h1 className="text-center text-3xl font-extrabold leading-9 tracking-tight sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+            <span>
+              We have <i>infinite </i>insights to share
+            </span>
           </h1>
           <p className="text-lg leading-7">{siteMetadata.description}</p>
         </div>
