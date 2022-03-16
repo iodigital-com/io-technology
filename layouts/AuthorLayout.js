27c819c93d68d7cf377523db935ae481eee67f01
Sander de Jong <sander@frontmen.nl>
2022-03-16 12:01:23 +0100

    prettier

--- layouts/AuthorLayout.js
+++ layouts/AuthorLayout.js
@@ -16,13 +16,7 @@ export default function AuthorLayout({ children, frontMatter }) {
         </div>
         <div className="items-start space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0">
           <div className="flex flex-col items-center space-x-2 pt-8">
-            <Image
-              src={avatar}
-              alt="avatar"
-              width="192px"
-              height="192px"
-              className="h-48 w-48"
-            />
+            <Image src={avatar} alt="avatar" width="192px" height="192px" className="h-48 w-48" />
             <h3 className="pt-4 pb-2 text-2xl font-bold leading-8 tracking-tight">{name}</h3>
             <div className="text-gray-500 dark:text-gray-400">{occupation}</div>
             <div className="text-gray-500 dark:text-gray-400">{company}</div>
