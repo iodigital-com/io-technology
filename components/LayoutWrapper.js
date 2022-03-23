b82542b3b0827e6ea3a17ffdb9cf591db6cbf3c9
Dave Bitter <dbitter@anwb.nl>
2022-03-23 14:25:03 +0100

    feat(navigation): add io-digital like navigation

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -1,3 +1,4 @@
+import { useEffect, useRef, useState } from 'react'
 import siteMetadata from '@/data/siteMetadata'
 import headerNavLinks from '@/data/headerNavLinks'
 import Logo from '@/data/logo.svg'
@@ -8,38 +9,64 @@ import MobileNav from './MobileNav'
 import ThemeSwitch from './ThemeSwitch'
 
 const LayoutWrapper = ({ children }) => {
+  const [scrolledPassedHeader, setScrolledPassedHeader] = useState(false)
+  const headerRef = useRef()
+  useEffect(() => {
+    const headerHeight = headerRef.current.scrollHeight
+    const handleScroll = () => {
+      setScrolledPassedHeader(window.scrollY >= headerHeight)
+    }
+
+    window.addEventListener('scroll', handleScroll)
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+
   return (
     <>
-      <header className="sticky top-0 z-50 flex items-center justify-between bg-io_blue-500 py-5 text-white">
-        <div>
-          <Link href="/" aria-label={siteMetadata.headerTitle}>
-            <div className="flex items-center justify-between">
-              <div className="mr-3">
-                <Logo />
-              </div>
-              {typeof siteMetadata.headerTitle === 'string' ? (
-                <div className="hidden h-6 text-2xl font-semibold sm:block">
-                  {siteMetadata.headerTitle}
+      <header
+        ref={headerRef}
+        className={`sticky top-0 z-50  ${
+          scrolledPassedHeader ? '' : 'bg-io_blue-500'
+        } py-4 px-4 text-white sm:bg-io_blue-500`}
+      >
+        <div className="container mx-auto flex items-center justify-between sm:justify-start">
+          <div>
+            <Link href="/" aria-label={siteMetadata.headerTitle}>
+              <div className="flex items-center justify-between">
+                <div
+                  className={`mr-3 ${
+                    scrolledPassedHeader ? 'opacity-0' : 'opacity-100'
+                  } duration-150 ease-out sm:opacity-100`}
+                >
+                  <Logo />
                 </div>
-              ) : (
-                siteMetadata.headerTitle
-              )}
-            </div>
-          </Link>
-        </div>
-        <div className="flex items-center text-base leading-5">
-          <div className="hidden sm:block">
-            {headerNavLinks.map((link) => (
-              <Link
-                key={link.title}
-                href={link.href}
-                className="p-1 font-medium text-white dark:text-gray-100 sm:p-4"
-              >
-                {link.title}
-              </Link>
-            ))}
+                {typeof siteMetadata.headerTitle === 'string' ? (
+                  <div className="hidden h-6 items-center text-xl font-semibold sm:flex xl:text-2xl">
+                    {siteMetadata.headerTitle}
+                  </div>
+                ) : (
+                  siteMetadata.headerTitle
+                )}
+              </div>
+            </Link>
           </div>
-          <MobileNav />
+          <div className="flex items-center text-base leading-5">
+            <div className="hidden items-center sm:flex">
+              {headerNavLinks.map((link) => (
+                <Link
+                  key={link.title}
+                  href={link.href}
+                  className="font-medium text-white dark:text-gray-100 sm:mt-2 sm:p-4"
+                >
+                  {link.title}
+                </Link>
+              ))}
+            </div>
+            <MobileNav />
+          </div>
         </div>
       </header>
       <div className="flex h-full flex-col justify-between">
