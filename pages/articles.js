1097120676478c6d5bc8207828bcbd86c0d88534
Sander de Jong <sander@frontmen.nl>
2022-04-11 09:58:10 +0200

    design articles/tags pages

--- pages/articles.js
+++ pages/articles.js
@@ -3,6 +3,7 @@ import siteMetadata from '@/data/siteMetadata'
 import ListLayout from '@/layouts/ListLayout'
 import { PageSEO } from '@/components/SEO'
 import { getAuthors } from '@/lib/authors'
+import { useBrandingTheme } from '@/lib/hooks/useBrandingTheme'
 
 export const POSTS_PER_PAGE = 10
 
@@ -15,10 +16,12 @@ export async function getStaticProps() {
   }
   const authors = await getAuthors(posts)
 
-  return { props: { initialDisplayPosts, posts, pagination, authors } }
+  return { props: { initialDisplayPosts, posts, pagination, authors, theme: 'rouge' } }
 }
 
 export default function Articles({ posts, initialDisplayPosts, pagination, authors }) {
+  const { theme } = useBrandingTheme()
+
   return (
     <>
       <PageSEO title={`Articles - ${siteMetadata.author}`} description={siteMetadata.description} />
@@ -26,8 +29,9 @@ export default function Articles({ posts, initialDisplayPosts, pagination, autho
         posts={posts}
         initialDisplayPosts={initialDisplayPosts}
         pagination={pagination}
-        title="All Posts"
+        title="All articles"
         authors={authors}
+        theme={theme}
       />
     </>
   )
