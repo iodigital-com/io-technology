c8a75a416d40dd70aa417066cdd790b856957f5b
Sander de Jong <sander@frontmen.nl>
2022-04-11 15:41:26 +0200

    get rid of talks page for now

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -2,7 +2,6 @@ const headerNavLinks = [
   { href: '/articles', title: 'Articles' },
   { href: '/videos', title: 'Videos' },
   { href: '/podcast', title: 'Podcast' },
-  { href: '/talks', title: 'Talks' },
 ]
 
 export default headerNavLinks
