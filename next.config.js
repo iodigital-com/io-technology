0015634e6202133de7934266fa7ced7617a714dd
Sander de Jong <sander@frontmen.nl>
2022-06-03 10:48:19 +0200

    fix: GTM not loading

--- next.config.js
+++ next.config.js
@@ -4,8 +4,6 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
-  default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline';
   style-src 'self' 'unsafe-inline';
   img-src * blob: data:;
   media-src 'none';
