0bd3e80a772a55caf0599395b746df3d5e419dc9
Mohammad Hossein Malek <mhos.malek@gmail.com>
2022-08-16 15:41:44 +0200

    :speech_balloon: update contribute page content

--- layouts/StaticPageLayout.js
+++ layouts/StaticPageLayout.js
@@ -13,16 +13,16 @@ export default function StaticPageLayout({ frontMatter, authorDetails, next, pre
     <>
       <PageSEO {...frontMatter} />
       <ScrollTop />
-      <article className="container mx-auto mb-72 max-w-xl pb-14 pt-10">
+      <article className="container mx-auto mx-auto mb-72 pt-8 pb-14 xl:pt-32">
         <div>
           <div
             className="divide-y divide-gray-200 pb-8 dark:divide-gray-700 xl:divide-y-0 "
             style={{ gridTemplateRows: 'auto 1fr' }}
           >
-            <h1 className="heading-title text-4xl font-medium xl:text-7xl">
+            <h1 className="heading-title  mb-8 text-center text-4xl font-medium xl:mb-24 xl:text-7xl">
               {<MarkdownRenderer markdown={title} />}
             </h1>
-            <div className="divide-y divide-gray-200 dark:divide-gray-700 xl:col-span-3 xl:row-span-2 xl:pb-0">
+            <div className="mx-auto max-w-xl divide-y divide-gray-200 dark:divide-gray-700 xl:col-span-3 xl:row-span-2 xl:pb-0">
               <div className="prose max-w-none pt-10 pb-8 dark:prose-dark">{children}</div>
             </div>
           </div>
