9ec6285d54f4f4225aa6ba961cef153d6de78575
Sander de Jong <sander@frontmen.nl>
2022-10-12 00:05:25 +0200

    feat: add experiments page

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -2,7 +2,7 @@ const headerNavLinks = [
   { href: '/articles', title: 'Articles' },
   { href: '/videos', title: 'Videos' },
   { href: '/talks', title: 'Talks' },
-  { href: '/podcast', title: 'Podcast' },
+  { href: '/experiments', title: 'Experiments' },
 ]
 
 export default headerNavLinks
