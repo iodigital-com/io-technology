b1c776695dc98a0d6648ef38e692a835243f123b
Sander de Jong <sander.dejong@iodigital.com>
2023-01-05 09:39:47 +0100

    chore: review comments

--- components/ContributorsGrid.js
+++ components/ContributorsGrid.js
@@ -4,7 +4,7 @@ import shuffle from '@/lib/shuffle'
 const ContributorsGrid = ({ contributors }) => {
   const shuffled = shuffle(contributors)
   return (
-    <div className="grid grid-cols-3 gap-y-4 md:grid-cols-4 md:gap-y-6 xl:grid-cols-5 xl:gap-y-8">
+    <div className="grid grid-cols-3 gap-y-8 md:grid-cols-4 md:gap-y-10 xl:grid-cols-5 xl:gap-y-12">
       {shuffled.map((contributor) => (
         <Contributor key={contributor.name} contributor={contributor} />
       ))}
