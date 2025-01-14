fd3f1f88f3f7353492ea7db47af1a1edc15e5041
Remy Parzinski <rs.parzinski@gmail.com>
2025-01-14 13:59:47 +0100

    Revert "chore: update major dependencies"
    This reverts commit bee0db804aa65ec116934a919b43daaec6d20e72.

--- .ncurc.major.cjs
+++ .ncurc.major.cjs
@@ -2,10 +2,5 @@ const minorConfig = require('./.ncurc.minor.cjs');
 
 module.exports = {
     ...minorConfig,
-    reject: [
-        ...minorConfig.reject,
-        'react', // @lottiefiles/react-lottie-player is incompatible with react 19
-        'react-dom', // https://github.com/LottieFiles/lottie-react/issues/168
-    ],
     target: 'latest',
 };
