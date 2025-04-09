b3cffb5236368aa9f7831b2819ce85b72618d8a5
Huss <961ahmad@gmail.com>
2025-04-09 17:45:25 +0200

    Update header to sticky. Fix icon issue on the search bar

--- layouts/ListLayout.js
+++ layouts/ListLayout.js
@@ -43,20 +43,22 @@ export default function ListLayout({
                       className="w-full px-5 py-5"
                       onChange={(e) => setSearchValue(e.target.value)}
                     />
-                    <svg
-                      className="absolute right-3 top-9 h-7 w-7 text-gray-400 dark:text-gray-300"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth={2}
-                        d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                      />
-                    </svg>
+                    {searchValue === '' && (
+                      <svg
+                        className="absolute right-3 top-9 h-7 w-7 text-gray-400 dark:text-gray-300"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth={2}
+                          d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                        />
+                      </svg>
+                    )}
                   </div>
                 </div>
               </div>
