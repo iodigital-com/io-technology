0ffd707684afe2621f56ecbcaef0f7933757240b
Alexey Ses <alexey.ses@iodigital.com>
2026-06-11 15:38:53 +0200

    feat(StaticPageLayout): remove redundant code related to heroImage

--- layouts/StaticPageLayout.tsx
+++ layouts/StaticPageLayout.tsx
@@ -1,5 +1,4 @@
 import { ReactNode } from 'react'
-import MarkdownRenderer from 'react-markdown-renderer'
 import { PageSEO } from '@/components/SEO'
 import ScrollTop from '@/components/ScrollTop'
 import type { FrontMatter } from '../types'
@@ -17,7 +16,6 @@ export default function StaticPageLayout({
   transparentHeader,
 }: StaticPageLayoutProps) {
   const { title, summary } = frontMatter
-  const heroImage = frontMatter.heroImage as string | undefined
   const heroTitle = frontMatter.heroTitle as string | undefined
   const heroDescription = frontMatter.heroDescription as string | undefined
 
@@ -38,12 +36,6 @@ export default function StaticPageLayout({
             className="divide-y divide-gray-200 pb-8 dark:divide-gray-700 xl:divide-y-0 "
             style={{ gridTemplateRows: 'auto 1fr' }}
           >
-            {/* Only show page title if no hero section */}
-            {!heroImage && (
-              <h1 className="heading-title  mb-8 text-center text-4xl font-medium xl:mb-24 xl:text-7xl">
-                <MarkdownRenderer markdown={title} />
-              </h1>
-            )}
             <div className="mx-auto max-w-xl divide-y divide-gray-200 dark:divide-gray-700 xl:col-span-3 xl:row-span-2 xl:pb-0">
               <div className="prose max-w-none pb-8 pt-10 dark:prose-dark">{children}</div>
             </div>
