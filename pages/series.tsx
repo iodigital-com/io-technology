d27fa3bd34dfe6c4a6a17c64d7cc013aaec5ab84
Alexey Ses <alexey.ses@iodigital.com>
2026-06-11 14:29:31 +0200

    feat(Series page): add dark background support

--- pages/series.tsx
+++ pages/series.tsx
@@ -1,14 +1,16 @@
 import siteMetadata from '@/data/siteMetadata'
 import SeriesLayout from '@/layouts/SeriesLayout'
 import { PageSEO } from '@/components/SEO'
-import { useBrandingTheme } from '@/lib/hooks/useBrandingTheme'
 import { getContentWithPagination } from '@/lib/hooks/useContentData'
 import type { ContentItem, AuthorsMap, PaginationMeta } from '../types'
+import SearchLayout from '@/layouts/SearchLayout'
+import HeroSection from '@/components/HeroSection'
+import { usePostSearch } from '@/lib/hooks/usePostSearch'
 
 export const SERIES_PER_PAGE = 10
 
 export async function getStaticProps() {
-  return getContentWithPagination('series', SERIES_PER_PAGE, 'blue', null, false)
+  return getContentWithPagination('series', SERIES_PER_PAGE, 'blue', null, true)
 }
 
 interface SeriesProps {
@@ -16,21 +18,32 @@ interface SeriesProps {
   initialDisplaySeries: ContentItem[]
   pagination: PaginationMeta
   authors: AuthorsMap
+  transparentHeader: boolean
 }
 
-export default function Series({ series, initialDisplaySeries, pagination, authors }: SeriesProps) {
-  const { theme } = useBrandingTheme()
+export default function Series({
+  series,
+  initialDisplaySeries,
+  pagination,
+  authors,
+  transparentHeader,
+}: SeriesProps) {
+  const { searchValue, setSearchValue, filteredPosts } = usePostSearch(series)
+  const displaySeries =
+    initialDisplaySeries.length > 0 && !searchValue ? initialDisplaySeries : filteredPosts
 
   return (
     <>
       <PageSEO title={`Series - ${siteMetadata.author}`} description={siteMetadata.description} />
+      <HeroSection title="Discover all series" isDarkBackground={transparentHeader}>
+        <SearchLayout onChange={setSearchValue} searchPlaceholder="Search series" />
+      </HeroSection>
       <SeriesLayout
-        series={series}
+        series={displaySeries}
         authors={authors}
-        initialDisplaySeries={initialDisplaySeries}
         pagination={pagination}
-        title="Discover all series"
-        theme={theme}
+        subpath="series"
+        searchValue={searchValue}
       />
     </>
   )
