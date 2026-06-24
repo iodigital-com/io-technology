f33ebb06b98c0696a011b8f14c132ce17cf8d551
Alexey Ses <alexey.ses@iodigital.com>
2026-06-24 14:12:35 +0200

    chore(scripts, package.json, tsconfig): replace "node" with "tsx" compiler, remove redundant .js files, convert two scripts to TypeScript and adjust TypeScript configs due to those changes

---
+++ data/headerNavLinks.ts
@@ -0,0 +1,9 @@
+const headerNavLinks = [
+  { href: '/articles', title: 'Articles' },
+  { href: '/videos', title: 'Videos' },
+  { href: '/talks', title: 'Talks' },
+  { href: '/workshops', title: 'Workshops' },
+  { href: '/experiments', title: 'Experiments' },
+]
+
+export default headerNavLinks
